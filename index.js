e89c05538fec9879d3963161029e889f1683ddfc
KilerBots <davidmersey707@gmail.com>
2024-12-29 19:04:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require("express")
 const os = require("os")
 const axios = require("axios")
 const { run } = require("shannz-playwright")
-
 const app = express()
 const PORT = process.env.PORT || 7860
-
 let totalHits = 0
 let hitsPerMinute = 0
 
@@ -29,26 +27,21 @@ memoryUsage: `${Math.round((os.totalmem() - os.freemem()) / 1024 / 1024)} MB use
 
 app.get("/generate", async (req, res) => {
 const { query } = req.query
-
 if (!query) {
 return res.status(400).json({ error: "Query parameter is required" })
 }
-
 let stats
 try {
 const result = await brat(query)
 const response = await axios.get(result.url, { responseType: 'arraybuffer' })
-
 res.set('Content-Type', 'image/webp')
 res.send(response.data)
-
 stats = 200
 } catch (error) {
 console.error(error)
 res.status(500).json({ error: "Failed to generate brat" })
 stats = 500
 }
-
 if (stats === 200) {
 hitsPerMinute++
 totalHits++
@@ -57,32 +50,26 @@ totalHits++
 
 app.get("/ssweb", async (req, res) => {
 const { link } = req.query
-
 if (!link) {
 return res.status(400).json({ error: "parameter link is required" })
 }
-
 let stats
 try {
-const result = await brat(link)
+const result = await ssweb(link)
 const response = await axios.get(result.url, { responseType: 'arraybuffer' })
-
 res.set('Content-Type', 'image/webp')
 res.send(response.data)
-
 stats = 200
 } catch (error) {
 console.error(error)
 res.status(500).json({ error: "Failed to generate brat" })
 stats = 500
 }
-
 if (stats === 200) {
 hitsPerMinute++
 totalHits++
 }
 })
-
 
 app.listen(PORT, () => {
 console.log(`Server running on http://localhost:${PORT}`)
@@ -95,11 +82,23 @@ hitsPerMinute = 0
 async function brat(query) {
 const code= "const { chromium } = require('playwright');\n\nasync function ssweb(q) {\nconst browser = await chromium.launch({ headless: true });\nconst page = await browser.newPage();\n\nawait page.goto(\"https://www.bratgenerator.com\");\nconst consentButtonSelector = \"#onetrust-accept-btn-handler\";\nif (await page.$(consentButtonSelector)) {\nawait page.click(consentButtonSelector);\nawait page.waitForTimeout(500);\n}\nawait page.click(\"#toggleButtonWhite\");\nawait page.fill(\"#textInput\", q);\nawait page.locator(\"#textOverlay\").screenshot({ path: 'brat.png' });\nconsole.log('success generate brat.png');\n\nawait browser.close();\n}\nssweb(`" + query + "`).then(a => console.log(a));"
 const result = await run("javascript", code)
-return "https://try.playwright.tech" + result.result.files[0].publicURL
+const files = result.result.files
+const fileData = files[0]
+return {
+url: "https://try.playwright.tech" + fileData.publicURL,
+fileName: fileData.fileName,
+extension: fileData.extension,
+}
 }
 
 async function ssweb(query) {
-const code = "from playwright.sync_api import sync_playwright\n\nwith sync_playwright() as p:\n    browser = p.chromium.launch()\n    page = browser.new_page()\n    page.goto(" + query +")\n    page.screenshot(path=\"example.png\")\n    browser.close()\n"
+const code = `from playwright.sync_api import sync_playwright\n\nwith sync_playwright() as p:\n    browser = p.chromium.launch()\n    page = browser.new_page()\n    page.goto(\"`+ query + `\")\n    page.screenshot(path=\"example.png\")\n    browser.close()\n`
 const result = await run("python", code)
-return "https://try.playwright.tech" + result.result.files[0].publicURL
+const files = result.result.files
+const fileData = files[0]
+return {
+url: "https://try.playwright.tech" + fileData.publicURL,
+fileName: fileData.fileName,
+extension: fileData.extension,
+}
 }
