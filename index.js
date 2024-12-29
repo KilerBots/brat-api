805cae3c3d75110110f4fc2c18ccf8ebdebfef2d
KilerBots <davidmersey707@gmail.com>
2024-12-29 18:21:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ return res.status(400).json({ error: "Query parameter is required" })
 let stats
 try {
 const result = await brat(query)
-const response = await axios.get(result, { responseType: 'arraybuffer' })
+const response = await axios.get(result.url, { responseType: 'arraybuffer' })
 
 res.set('Content-Type', 'image/webp')
 res.send(response.data)
@@ -64,8 +64,8 @@ return res.status(400).json({ error: "parameter link is required" })
 
 let stats
 try {
-const result = await ssweb(link)
-const response = await axios.get(result, { responseType: 'arraybuffer' })
+const result = await brat(link)
+const response = await axios.get(result.url, { responseType: 'arraybuffer' })
 
 res.set('Content-Type', 'image/webp')
 res.send(response.data)
@@ -73,7 +73,7 @@ res.send(response.data)
 stats = 200
 } catch (error) {
 console.error(error)
-res.status(500).json({ error: "Failed to screenshoot web" })
+res.status(500).json({ error: "Failed to generate brat" })
 stats = 500
 }
 
@@ -82,6 +82,7 @@ hitsPerMinute++
 totalHits++
 }
 })
+
 
 app.listen(PORT, () => {
 console.log(`Server running on http://localhost:${PORT}`)
@@ -94,17 +95,11 @@ hitsPerMinute = 0
 async function brat(query) {
 const code= "const { chromium } = require('playwright');\n\nasync function ssweb(q) {\nconst browser = await chromium.launch({ headless: true });\nconst page = await browser.newPage();\n\nawait page.goto(\"https://www.bratgenerator.com\");\nconst consentButtonSelector = \"#onetrust-accept-btn-handler\";\nif (await page.$(consentButtonSelector)) {\nawait page.click(consentButtonSelector);\nawait page.waitForTimeout(500);\n}\nawait page.click(\"#toggleButtonWhite\");\nawait page.fill(\"#textInput\", q);\nawait page.locator(\"#textOverlay\").screenshot({ path: 'brat.png' });\nconsole.log('success generate brat.png');\n\nawait browser.close();\n}\nssweb(`" + query + "`).then(a => console.log(a));"
 const result = await run("javascript", code)
-
-const files = result.result.files
-const fileData = files[0]
-
 return "https://try.playwright.tech" + result.result.files[0].publicURL
-
 }
 
 async function ssweb(query) {
 const code = "from playwright.sync_api import sync_playwright\n\nwith sync_playwright() as p:\n    browser = p.chromium.launch()\n    page = browser.new_page()\n    page.goto(" + query +")\n    page.screenshot(path=\"example.png\")\n    browser.close()\n"
 const result = await run("python", code)
-
 return "https://try.playwright.tech" + result.result.files[0].publicURL,
 }
